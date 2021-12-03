40bf43c7047b4eaee9c9dec5fee77e55070b985d
Hazurl <ricoujules69@gmail.com>
2021-12-03 09:04:42 +0100

    Day 3 part 2

--- 2021/src/day3.rs
+++ 2021/src/day3.rs
@@ -32,6 +32,52 @@ fn part1(bins: &std::vec::Vec<String>) -> u64 {
 	gamma * (!gamma & (1 << input_size) - 1)
 }
 
+fn rating_from_bit_criteria<F: Fn(usize, usize) -> char>(
+	bins: std::vec::Vec<String>,
+	bit_criteria: F,
+) -> u64 {
+	let input_size = bins[0].len();
+	(0..input_size).fold(bins.clone(), |values, index| {
+		if values.len() == 1 {
+			return values;
+		}
+		let count1 = values
+			.iter()
+			.map(|v| v.chars().nth(index).unwrap())
+			.filter(|c| *c == '1')
+			.count();
+
+		let to_keep = bit_criteria(count1, values.len());
+		values
+			.iter()
+			.filter(|v| v.chars().nth(index).unwrap() == to_keep)
+			.cloned()
+			.collect()
+	})[0]
+		.chars()
+		.enumerate()
+		.map(|(index, digit)| (digit.to_digit(10).unwrap() as u64) << (input_size - index - 1))
+		.sum()
+}
+
 fn part2(bins: &std::vec::Vec<String>) -> u64 {
-	0
+	rating_from_bit_criteria(
+		bins.clone(),
+		|count1, size| {
+			if count1 * 2 >= size {
+				'1'
+			} else {
+				'0'
+			}
+		},
+	) * rating_from_bit_criteria(
+		bins.clone(),
+		|count1, size| {
+			if count1 * 2 < size {
+				'1'
+			} else {
+				'0'
+			}
+		},
+	)
 }
