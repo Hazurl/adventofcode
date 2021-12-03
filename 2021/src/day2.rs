e1746f81f405e2d5fecaed926aabd851eef1eb9f
Hazurl <ricoujules69@gmail.com>
2021-12-03 08:38:47 +0100

    Remove warnings on day2

--- 2021/src/day2.rs
+++ 2021/src/day2.rs
@@ -1,5 +1,4 @@
 use crate::utility;
-use itertools::*;
 
 fn parse_file(base_path_inputs: &str, filename: &str) -> std::vec::Vec<String> {
 	utility::read_lines(format!("{}/day2/{}.txt", base_path_inputs, filename))
@@ -38,25 +37,24 @@ fn part1(cmds: &std::vec::Vec<String>) -> i64 {
 }
 
 fn part2(cmds: &std::vec::Vec<String>) -> i64 {
-	let (horizontal, depth, aim) =
-		cmds
-			.into_iter()
-			.fold((0, 0, 0), |(horizontal, depth, aim), cmd| {
-				let arg = cmd
-					.chars()
-					.skip_while(|c| *c != ' ')
-					.skip(1)
-					.collect::<String>()
-					.parse::<i64>()
-					.unwrap();
+	let (horizontal, depth, _) = cmds
+		.into_iter()
+		.fold((0, 0, 0), |(horizontal, depth, aim), cmd| {
+			let arg = cmd
+				.chars()
+				.skip_while(|c| *c != ' ')
+				.skip(1)
+				.collect::<String>()
+				.parse::<i64>()
+				.unwrap();
 
-				match cmd.chars().nth(0).unwrap() {
-					'f' => (horizontal + arg, depth + aim * arg, aim),
-					'u' => (horizontal, depth, aim - arg),
-					'd' => (horizontal, depth, aim + arg),
-					_ => panic!("Unknown command"),
-				}
-			});
+			match cmd.chars().nth(0).unwrap() {
+				'f' => (horizontal + arg, depth + aim * arg, aim),
+				'u' => (horizontal, depth, aim - arg),
+				'd' => (horizontal, depth, aim + arg),
+				_ => panic!("Unknown command"),
+			}
+		});
 
 	horizontal * depth
 }
